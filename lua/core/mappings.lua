ec0ff537f3eb8d7e260311b91b7ae6cae06f5ad0
POP303U <nicoanton.gering@gmail.com>
2023-11-25 11:50:54 +0100

    Update: Remove redundant options/mappings

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -1,40 +1,37 @@
--- variable for conciseness
-local keymap = vim.keymap
-local current_dir = vim.fn.getcwd()
-
 vim.g.mapleader = ' '
 
--- general keymaps
-keymap.set('n', '<leader>nh', ':nohl<CR>')
-keymap.set('n', 'x', '"_x')
+vim.keymap.set('n', '<leader>nh', ':nohl<CR>')
+vim.keymap.set('n', 'x', '"_x')
 
-keymap.set('n', '<leader>+', '<c-a>')       -- increment selected number
-keymap.set('n', '<leader>-', '<c-x>')       -- decrement selected number
+vim.keymap.set('n', '<leader>+', '<c-a>')       -- increment selected number
+vim.keymap.set('n', '<leader>-', '<c-x>')       -- decrement selected number
+vim.keymap.set('n', '<leader>sv', '<c-w>v')     -- split window vertically
+vim.keymap.set('n', '<leader>sh', '<c-w>s')     -- split window horizontally
+vim.keymap.set('n', '<leader>se', '<c-w>=')     -- make split windows equal width (if changed)
 
-keymap.set('n', '<leader>sv', '<c-w>v')     -- split window vertically
-keymap.set('n', '<leader>sh', '<c-w>s')     -- split window horizontally
-keymap.set('n', '<leader>se', '<c-w>=')     -- make split windows equal width (if changed)
-keymap.set('n', '<leader>sx', ':close<cr>') -- close current split window
+vim.keymap.set('n', '<leader>sx', ':close<cr>') -- close current split window
+vim.keymap.set('t', '<C-n>', "<C-\\><C-n>") -- Get into normal mode with current terminal
 
 -- the best remap ever
-keymap.set('v', 'K', ":m '>-2<cr>gv=gv")
-keymap.set('v', 'J', ":m '>+1<cr>gv=gv")
+vim.keymap.set('v', 'K', ":m '>-2<cr>gv=gv")
+vim.keymap.set('v', 'J', ":m '>+1<cr>gv=gv")
 
 -- fixes this stupid issue where cursor goes to end of line with shift-j
-keymap.set('n', 'J', 'mzJ`z')
+vim.keymap.set('n', 'J', 'mzJ`z')
 
 -- paste into system clipboard
-keymap.set('n', '<leader>y', "\"+y")
-keymap.set('v', '<leader>y', "\"+y")
+vim.keymap.set('n', '<leader>y', "\"+y")
+vim.keymap.set('v', '<leader>y', "\"+y")
 
 -- renaming good
-keymap.set("n", "<leader>rw", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set("n", "<leader>rw", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
 -- neogit
-keymap.set('n', '<leader>g', ':Neogit kind=vsplit<CR>')
+vim.keymap.set('n', '<leader>g', ':Neogit kind=vsplit<CR>')
 
 -- Lazy
-keymap.set('n', '<leader>l', ':Lazy<CR>')
+vim.keymap.set('n', '<leader>l', ':Lazy<CR>')
 
 -- Undotree
-keymap.set('n', '<leader>ut', vim.cmd.UndotreeToggle)
+vim.keymap.set('n', '<leader>ut', vim.cmd.UndotreeToggle)
+
--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,72 +1,70 @@
 -- variable for conciseness
-local options    = vim.opt
-
-options.backup   = false
-options.swapfile = false
+vim.options.backup   = false
+vim.options.swapfile = false
 
 -- check for windows and set terminal_emulator
-options.undofile = true
+vim.options.undofile = true
 
 if package.config:sub(1, 1) == "/" then -- doesn't work with windows lol
-    options.undodir = os.getenv("HOME") .. "/.vim/undodir"
+    vim.options.undodir = os.getenv("HOME") .. "/.vim/undodir"
 end
 
 -- better searching
-options.hlsearch       = false
-options.incsearch      = true
+vim.options.hlsearch       = false
+vim.options.incsearch      = true
 
 -- numbers
-options.number         = true
-options.relativenumber = true
+vim.options.number         = true
+vim.options.relativenumber = true
 
 -- chonky cursor
-options.guicursor      = ""
+vim.options.guicursor      = ""
 
 -- needed config
-options.backspace      = '2'
-options.showcmd        = true
-options.laststatus     = 2
-options.autowrite      = true
-options.autoread       = true
-options.colorcolumn    = "80"
+vim.options.backspace      = '2'
+vim.options.showcmd        = true
+vim.options.laststatus     = 2
+vim.options.autowrite      = true
+vim.options.autoread       = true
+vim.options.colorcolumn    = "80"
 
 -- tabs & indentation
-options.tabstop        = 4
-options.shiftwidth     = 4
-options.softtabstop    = 4
-options.expandtab      = true
-options.autoindent     = true
+vim.options.tabstop        = 4
+vim.options.shiftwidth     = 4
+vim.options.softtabstop    = 4
+vim.options.expandtab      = true
+vim.options.autoindent     = true
 
 -- line wrapping
-options.wrap           = false
+vim.options.wrap           = false
 
 -- search settings
-options.ignorecase     = true
-options.smartcase      = true
+vim.options.ignorecase     = true
+vim.options.smartcase      = true
 
 -- set scrolling limit
-options.scrolloff      = 8
+vim.options.scrolloff      = 8
 
 -- blazingly fast update time
-options.updatetime     = 50
+vim.options.updatetime     = 50
 
 -- appearance
-options.background     = "dark"
+vim.options.background     = "dark"
 
 -- split windows
-options.splitright     = true
-options.splitbelow     = true
+vim.options.splitright     = true
+vim.options.splitbelow     = true
 
--- indenting options
-options.autoindent     = true -- auto indent
-options.smartindent    = true -- smart indent
-options.tabstop        = 4
-options.shiftwidth     = 4
-options.expandtab      = true
+-- indenting vim.options
+vim.options.autoindent     = true -- auto indent
+vim.options.smartindent    = true -- smart indent
+vim.options.tabstop        = 4
+vim.options.shiftwidth     = 4
+vim.options.expandtab      = true
 
 -- true color 24-bit (for terminals that support it)
-options.termguicolors  = true
+vim.options.termguicolors  = true
 
 -- fix word detection
-options.iskeyword:append("-")
-options.iskeyword:append("_") -- probably unnecessary :)
+vim.options.iskeyword:append("-")
+vim.options.iskeyword:append("_") -- probably unnecessary :)
