41849418550f478ff9781e3730a7b1822dca661a
POP303U <nicoanton.gering@gmail.com>
2023-12-11 22:35:22 +0100

    Update: More Transparency for other items

--- lua/core/plugins/rose-pine.lua
+++ lua/core/plugins/rose-pine.lua
@@ -2,3 +2,5 @@ vim.cmd.colorscheme("rose-pine")
 -- idontwanttransparency.sh
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
+vim.api.nvim_set_hl(0, "Pmenu", { bg = "none" })
