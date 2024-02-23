37a60029059ab485c47dfdfb5f30b988a33e535e
POP303U <nicoanton.gering@gmail.com>
2024-02-23 23:31:57 +0100

    Feat: Make background transparent

--- lua/core/plugins/colorscheme.lua
+++ lua/core/plugins/colorscheme.lua
@@ -1,9 +1,8 @@
 vim.cmd.colorscheme("rose-pine")
+vim.opt.background = "dark"
 -- idontwanttransparency.sh
 
---[[
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
 vim.api.nvim_set_hl(0, "Pmenu", { bg = "none" })
-]]--
