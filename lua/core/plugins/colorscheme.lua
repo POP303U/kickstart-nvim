a13947789dc50a3da80e985b0729bf86570c2281
POP303U <nicoanton.gering@gmail.com>
2024-02-26 17:56:03 +0100

    Update: Ignore neovide

--- lua/core/plugins/colorscheme.lua
+++ lua/core/plugins/colorscheme.lua
@@ -2,7 +2,9 @@ vim.cmd.colorscheme("rose-pine")
 vim.opt.background = "dark"
 -- idontwanttransparency.sh
 
-vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
-vim.api.nvim_set_hl(0, "Pmenu", { bg = "none" })
+if not vim.g.neovide then
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
+    vim.api.nvim_set_hl(0, "Pmenu", { bg = "none" })
+end
